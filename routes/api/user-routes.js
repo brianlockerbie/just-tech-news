e26e4fdc90e27814240db956c026407b600ce65a
Brian <brian.lockerbie@gmail.com>
2022-06-29 23:40:23 -0700

    added attributes to the query

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,14 +2,22 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET /api/users
-router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll()
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+User.findOne({
+  attributes: { exclude: ['password'] },
+  where: {
+    id: req.params.id
+  }
+})
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   });
 
 // GET /api/users/1
