4a2cebd51ccb36e7074526faba9dfed7a96cc0a8
Brian <brian.lockerbie@gmail.com>
2022-07-23 18:36:08 -0700

    added partials

---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+module.exports = {
+    format_date: date => {
+      // same logic as before...
+    },
+    format_url: url => {
+        return url
+          .replace('http://', '')
+          .replace('https://', '')
+          .replace('www.', '')
+          .split('/')[0]
+          .split('?')[0];
+    },
+    format_plural: (word, amount) => {
+      if (amount !== 1) {
+        return `${word}s`;
+      }
+  
+      return word;
+    }
+  }
